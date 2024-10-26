86e28084415dfd8e4270befb39787018095f1b32
Awindsr <84223604+awindsr@users.noreply.github.com>
2024-10-26 12:51:50 +0530

    fix: remove unused imports

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,16 +1,11 @@
-import ProductsFilter from '@/components/products/ProductsFilter'
+
 import ProductsList from '@/components/products/ProductsList'
-import ProductsNavbar from '@/components/products/ProductsNavbar'
 import React from 'react'
 
 export default function page() {
   return (
     <div className='min-h-screen flex flex-col'>
-        <div>
-        {/* <ProductsNavbar/> */}
-        </div>
         <div className='flex items-start justify-between space-x-4 w-full h-full'>
-            {/* <ProductsFilter/> */}
             <ProductsList/>
 
         </div>
