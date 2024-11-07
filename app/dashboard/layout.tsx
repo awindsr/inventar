d07f1c70fd55c75f12eb3736598ef74c855b7f31
Awindsr <84223604+awindsr@users.noreply.github.com>
2024-11-07 20:27:18 +0530

    update: app/dashboard/layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -28,12 +28,29 @@ export default function DashboardLayout({
     );
   }
 
+
+
   return (
-    <div className="min-h-screen bg-custom-gradient">
+    <>
+      <div className='md:hidden  min-h-screen w-full bg-custom-gradient space-y-6 flex flex-col justify-center items-center'>
+        <div className='flex flex-col '>
+          <img src='/logo.png' alt='logo' className='w-20 h-20 mx-auto' />
+          <p>
+            <span className='text-4xl font-bold text-white font-raleway'>Inventar</span>
+            <span className='text-4xl font-bold text-green-500 font-raleway'>.</span>
+          </p>
+        </div>
+        <p className='text-xl font-light text-white font-raleway w-[50vw]'>
+          Please use a desktop to access this app.
+        </p>
+      </div>
+      
+      <div className="hidden md:block min-h-screen bg-custom-gradient">
       <Navbar />
       <main className="p-8">
         {children}
       </main>
     </div>
+    </>
   );
 }
